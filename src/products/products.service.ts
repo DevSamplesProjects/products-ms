7528c5aff01223b588e97009fc0b3fce0d534f18
DanMiramontes <dmiramontesg@gmial.com>
2025-11-23 00:50:20 -0600

    Integration validate-products

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,9 +1,10 @@
-import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { HttpStatus, Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
 import { PaginationDto } from '../common/dto/pagination.dto';
 import { NotFoundError } from 'rxjs';
+import { RpcException } from '@nestjs/microservices';
 
 
 @Injectable()
@@ -75,5 +76,26 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     });
 
     return product;
+  }
+  async validateProducts(ids: number[]) {
+    ids = Array.from(new Set(ids));
+
+    const products = await this.product.findMany({
+      where: {
+        id: {
+          in: ids
+        }
+      }
+    });
+
+    if ( products.length !== ids.length ) {
+      throw new RpcException({
+        message: 'Some products were not found',
+        status: HttpStatus.BAD_REQUEST,
+      });
+    }
+
+    return products;
+
   }
 }
